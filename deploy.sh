6edced4ea896b619410b2831ad27b8108a713a3c
AlexMost <alexmost1989@gmail.com>
2015-01-22 23:03:49 +0200

    improve deploy

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
-gulp deploy && 
-mv dist ../ &&
+# gulp deploy &&
+rm -rf ../dist &&
+mv dist ../dist &&
 git checkout gh-pages &&
-mv dist ../dist . &&
+mv dist ../dist/* . &&
+rm -r ../dist &&
 git add . &&
 git push
